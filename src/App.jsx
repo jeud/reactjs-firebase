9bee712b5864a7341a64c6795c85fdf77954413e
Kongthap Thammachat <jeud@MacBook-Pro.local>
2019-05-24 10:36:58 +0700

    update router structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import React, { useEffect } from 'react'
 import 'bulma/css/bulma.min.css'
 
-import firebaseApp from './firebase'
+import Navbar from './Navbar'
+import Routes from './Routes'
+
+// import firebaseApp from './firebase'
 
 export default function App() {
     useEffect(() => {
-        console.log(firebaseApp)
+        // console.log(firebaseApp)
     }, [])
 
     return <div>
         <section className="section">
             <div className="container">
                 <h3 className="title">App</h3>
+                <hr />
+                <Navbar />
+                <hr />
+                <Routes />
             </div>
         </section>
     </div>
---
+++ src/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Navbar() {
+    return <div>
+        <h3 className="title">Navbar</h3>
+    </div>
+}
