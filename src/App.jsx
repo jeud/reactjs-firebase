b00a32aa6fda97ad74ebe43e5d9cde1af3285f8c
Kongthap Thammachat <jeud@MacBook-Pro.local>
2019-05-24 10:16:50 +0700

    update firebase config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import 'bulma/css/bulma.min.css'
 
+import firebaseApp from './firebase'
+
 export default function App() {
+    useEffect(() => {
+        console.log(firebaseApp)
+    }, [])
+
     return <div>
         <section className="section">
             <div className="container">
--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,13 @@ import '@firebase/firestore'
 const firebaseApp = firebase.default
 
 const config = {
-
+    apiKey: 'AIzaSyCwpPzNyJ2SEXL6drYMEqVZNTGAPwIU7as',
+    authDomain: 'tutor4dev-firestore.firebaseapp.com',
+    databaseURL: 'https://tutor4dev-firestore.firebaseio.com',
+    projectId: 'tutor4dev-firestore',
+    storageBucket: 'tutor4dev-firestore.appspot.com',
+    messagingSenderId: '370882667514',
+    appId: '1:370882667514:web:b9b61a2e5198fbc9'
 }
 
-export default firebaseApp.initializeApp(config)
+export default firebaseApp.apps[0] || firebaseApp.initializeApp(config)
