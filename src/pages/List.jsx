bb57d888dffb6ce3ae9c3c7d7a6f274ff92d1dc1
Kongthap Thammachat <jeud@MacBook-Pro.local>
2019-05-24 13:06:57 +0700

    update sweetalert

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,27 +1,43 @@
 import React, { useState, useEffect } from 'react'
 import firebaseApp from '../firebase'
+import Swal from 'sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
 export default function List() {
     const [users, setUsers] = useState([])
 
     useEffect(() => {
-        const userCollection = firebaseApp.firestore().collection('users')
+        // const userCollection = firebaseApp.firestore().collection('users')
 
-        userCollection.get()
-            .then(querySnapshot => {
-                const users = querySnapshot.docs.map(each => {
-                    return {
-                        id: each.id,
-                        ...each.data()
-                    }
-                })
+        // userCollection.get()
+        //     .then(querySnapshot => {
+        //         const users = querySnapshot.docs.map(each => {
+        //             return {
+        //                 id: each.id,
+        //                 ...each.data()
+        //             }
+        //         })
 
-                setUsers(users)
-            })
+        //         setUsers(users)
+        //     })
     }, [])
+
+    function handleDelete() {
+        // Swal.fire({
+        //     title: 'Error!',
+        //     text: 'Do you want to continue',
+        //     type: 'error',
+        //     confirmButtonText: 'Cool'
+        // })
+        //     .then(xxx => {
+        //         console.log(xxx)
+        //     })
+        alert('You clicked the button!')
+    }
 
     return <div>
         <h3 className="title">List</h3>
+        <button onClick={handleDelete}>Delete</button>
         <table className="table">
             <tbody>
                 {users.map(each => {
@@ -29,7 +45,7 @@ export default function List() {
                         <td>{each.id}</td>
                         <td>{each.firstName}</td>
                         <td><button className="button is-warning">Edit</button></td>
-                        <td><button className="button is-danger">Delete</button></td>
+                        <td><button onClick={handleDelete} className="button is-danger">Delete</button></td>
                     </tr>
                 })}
             </tbody>
