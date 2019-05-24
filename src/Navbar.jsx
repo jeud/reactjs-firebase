a52a4e87e1cc551f6a96fab36ee8c0195f93f9cd
Kongthap Thammachat <jeud@MacBook-Pro.local>
2019-05-24 15:44:51 +0700

    update protect route

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import firebaseApp from './firebase'
+
 export default function Navbar(props) {
+
+    function handleLogout() {
+        firebaseApp.auth().signOut()
+    }
+
     return <div>
         <Link to="/">Create</Link>&nbsp;|&nbsp;
         <Link to="/list">List</Link>&nbsp;|&nbsp;
 
         {props.isAuth
             && <p>
-                <button className="button is-danger">Logout</button>
+                <button onClick={handleLogout} className="button is-danger">Logout</button>
             </p>
         }
     </div>
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 import Create from './pages/Create'
 import List from './pages/List'
 import Edit from './pages/Edit'
 import Login from './pages/Login'
 
+function ProtectRoute(props) {
+    const isAuth = true
+    const { component: Component } = props
+
+    return isAuth ? <Component /> : <Redirect to="/login" />
+}
+
 export default function Routes() {
     return <div>
         <Switch>
             <Route path="/" exact component={Create} />
-            <Route path="/list" component={List} />
+            {/* <Route path="/list" component={List} /> */}
+            <ProtectRoute path="/list" component={List} />
             <Route path="/edit/:id" component={Edit} />
             <Route path="/login" component={Login} />
         </Switch>
