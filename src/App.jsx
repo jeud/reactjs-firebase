341b0188578f03a9c8c3fe4bc697ae9da0aa6c60
Kongthap Thammachat <jeud@MacBook-Pro.local>
2019-05-24 15:10:25 +0700

    authen subscription

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import React, { useEffect } from 'react'
 import 'bulma/css/bulma.min.css'
 
+import firebaseApp from './firebase'
 import Navbar from './Navbar'
 import Routes from './Routes'
 
-// import firebaseApp from './firebase'
-
 export default function App() {
     useEffect(() => {
-        // console.log(firebaseApp)
+        const unsubscribe = firebaseApp.auth().onAuthStateChanged(user => {
+            if (user) {
+                console.log('logged in')
+            } else {
+                console.log('logout')
+            }
+        })
+
+        return () => {
+            unsubscribe()
+        }
     }, [])
 
     return <div>
