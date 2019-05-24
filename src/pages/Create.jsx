2482c313b1d6dfd90d54310aea9c1b0848d2ba55
Kongthap Thammachat <jeud@MacBook-Pro.local>
2019-05-24 11:00:35 +0700

    update user state

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Create() {
+    const [user, setUser] = useState({})
+
+    function handleChange(e) {
+        const fieldName = e.target.name
+
+        setUser({
+            [fieldName]: e.target.value
+        })
+
+        console.log(user)
+    }
+
     return <div>
         <h3 className="title">Create</h3>
+
+        <div className="field">
+            <input onChange={handleChange} type="text" className="input" name="firstName" />
+        </div>
+
+        <div className="field">
+            <input onChange={handleChange} type="text" className="input" name="lastName" />
+        </div>
+
+        <p><button className="button">Save</button></p>
     </div>
 }
