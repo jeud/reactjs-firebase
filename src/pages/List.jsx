8e80fe94b9a11fc65aa735a4a5204a4bd279064f
Kongthap Thammachat <jeud@MacBook-Pro.local>
2019-05-24 13:45:59 +0700

    update router link

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,26 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import firebaseApp from '../firebase'
 import Swal from 'sweetalert2'
 import 'sweetalert2/dist/sweetalert2.min.css'
 
 export default function List() {
     const [users, setUsers] = useState([])
-
-    // useEffect(() => {
-    //     const userCollection = firebaseApp.firestore().collection('users')
-
-    // userCollection.get()
-    //     .then(querySnapshot => {
-    //         const users = querySnapshot.docs.map(each => {
-    //             return {
-    //                 id: each.id,
-    //                 ...each.data()
-    //             }
-    //         })
-
-    //         setUsers(users)
-    //     })
-    // }, [])
 
     useEffect(() => {
         const userCollection = firebaseApp.firestore().collection('users')
@@ -72,7 +57,7 @@ export default function List() {
                     return <tr key={each.id}>
                         <td>{each.id}</td>
                         <td>{each.firstName}</td>
-                        <td><button className="button is-warning">Edit</button></td>
+                        <td><Link to={`/edit/${each.id}`} className="button is-warning">Edit</Link></td>
                         <td><button onClick={() => handleDelete(each.id)} className="button is-danger">Delete</button></td>
                     </tr>
                 })}
